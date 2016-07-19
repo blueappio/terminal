9dabc3ff15c24cdb52b459b23990a87bed01b0a3
MALLIKARJUNA <mallikarjuna.j@vensi.com>
2016-07-20 01:42:05 +0530

    Updated the app

--- app.js
+++ app.js
@@ -1,91 +1,227 @@
-var app;
-(function(){
-  app = angular.module('terminal', ['ngMaterial'])
-  .config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-      .primaryPalette('blue')
-      .accentPalette('pink');
-    $mdThemingProvider.theme('success-toast');
-    $mdThemingProvider.theme('error-toast');
-    
-    $mdThemingProvider.alwaysWatchTheme(true);
-  })
+"use strict";
 
+var app;
+(function () {
+    app = angular.module('terminal', ['ngMaterial', 'ngMdIcons'])
+        .config(function ($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('blue')
+                .accentPalette('pink');
+            $mdThemingProvider.theme('success-toast');
+            $mdThemingProvider.theme('error-toast');
+            $mdThemingProvider.alwaysWatchTheme(true);
+        })
 })();
 
-app.controller('mainController', function($scope, $mdToast){
+app.run(['$document', '$window', function($document, $window) {
+    var document = $document[0];
+    document.addEventListener('click', function(event) {
+        var hasFocus = document.hasFocus();
+        if (!hasFocus) $window.focus();
+    });
+}]);
 
-    $scope.terminal = terminal;
-    $scope.disable = true;
+app.service('terminalService', function () {
+    return new Terminal(navigator.bluetooth);
+});
 
-    function connectedWithDevice(){
-      $scope.disable = false;
-      $scope.terminal.onSuccess('connected with device');
-    }
+app.controller('mainController', function ($scope, $mdToast, $mdDialog, terminalService) {
 
-    function dataWritten(){
-      console.log('Write successfully');
-      $scope.terminal.terminaldata = $scope.terminal.terminaldata + ' <' +$scope.inputData + '>';
-      $scope.inputData = '';
-      $scope.$apply();
-    }
+    $scope.terminal = terminalService;
+    $scope.terminaldata = '';
 
-    $scope.terminal.onSuccess = function(message){
+    function goodToast(message) {
         $mdToast.show(
-          $mdToast.simple()
-            .content(message)
-            .position('top right')
-            .hideDelay(2500)
-            .theme("success-toast")
+            $mdToast.simple()
+                .textContent(message)
+                .position('top')
+                .theme("success-toast")
+                .hideDelay(2500)
         );
     };
 
-    $scope.terminal.onError = function(message){
+    function badToast(message) {
         $mdToast.show(
-          $mdToast.simple()
-            .content(message)
-            .position('top right')
-            .hideDelay(2500)
-            .theme("error-toast")
+            $mdToast.simple()
+                .textContent(message)
+                .position('top')
+                .theme('error-toast')
+                .hideDelay(2500)
         );
     };
 
-    $scope.terminal.updateUI = function(){
+    function showLoadingIndicator($event, text) {
+        var parentEl = angular.element(document.body);
+        $mdDialog.show({
+            parent: parentEl,
+            targetEvent: $event,
+            clickOutsideToClose: false,
+            template: '<md-dialog style="width: 250px;top:95px;margin-top: -170px;" aria-label="loadingDialog" ng-cloak>' +
+            '<md-dialog-content>' +
+            '<div layout="row" layout-align="center" style="padding: 40px;">' +
+                '<div style="padding-bottom: 20px;">' +
+                    '<md-progress-circular class="md-accent md-hue-1" md-mode="indeterminate" md-diameter="40" style="right: 20px;bottom: 10px;">' +
+                    '</md-progress-circular>' +
+                '</div>' +
+            '</div>' +
+            '<div layout="row" layout-align="center" style="padding-bottom: 20px;">' +
+            '<label>' + text + '</label>' +
+            '</div>' +
+            '</md-dialog-content>' +
+            '</md-dialog>',
+            locals: {
+                items: $scope.items
+            },
+            controller: DialogController
+        });
+        function DialogController($scope, $mdDialog, items) {
+            $scope.items = items;
+            $scope.closeDialog = function () {
+                $mdDialog.hide();
+            }
+        }
+    }
+
+    function dismissLoadingIndicator() {
+        $mdDialog.cancel();
+    };
+
+    $scope.terminal.updateUI = function (value) {
+        var tmpArray = new Uint8Array(value.buffer);
+        for (var i = 0; i < tmpArray.length; i++) {
+            $scope.terminaldata = $scope.terminaldata + tmpArray[i].toString(16);
+        }
         $scope.$apply();
     };
 
-    $scope.sendClick = function () {
-      console.log($scope.inputData);
-      if ($scope.inputData === undefined) {
-          $scope.terminal.onError('Enter Command to Send');
-      } else if ($scope.inputData === ''){
-          $scope.terminal.onError('Enter Command to Send');
-      }else{
-          terminal.writeData($scope.inputData.trim()).then(function () {
-            return dataWritten();
-          });
-      }      
+    $scope.showSettingsDialog = function (ev) {
+        $mdDialog.show({
+                controller: SettingsController,
+                templateUrl: 'settings.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            })
+            .then(function (answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function () {
+                $scope.status = 'You cancelled the dialog.';
+            });
     };
 
-    $scope.connectClick = function () {
-        $scope.terminal.onSuccess('Connecting ....');
-        terminal.connect().then(function () {
-            return terminal.getConfigData().then(function (configData) {
-                return terminal.configData = configData;
-            }).then(function () {
-                return connectedWithDevice();
-            });
-        }).catch(function (error) {
-            console.error('Argh!', error, error.stack ? error.stack : '');
-        });
+    $scope.onClear = function () {
+        $scope.terminaldata = '';
     }
 
-    if (navigator.bluetooth == undefined) {
-        console.log("No navigator.bluetooth found.");
-        $scope.terminal.onError("No navigator.bluetooth found.");
+    $scope.onSend = function () {
+        if ($scope.inputData === undefined) {
+            badToast('Enter Command to Send');
+            return;
+        }
+
+        if ($scope.inputData === '') {
+            badToast('Enter Command to Send');
+            return;
+        }
+
+        var bytes = [];
+        for (var i = 0; i < $scope.inputData.length; i = i + 2) {
+            bytes.push(parseInt($scope.inputData[i] + $scope.inputData[i + 1], 16));
+        }
+        var bufView = new Uint8Array(bytes);
+
+        $scope.terminal.writeData(bufView)
+            .then(function () {
+                $scope.terminaldata = $scope.terminaldata + ' <' + $scope.inputData + '>';
+                $scope.inputData = '';
+                $scope.$apply();
+            })
+            .catch(function (error) {
+                badToast('Unable to send data.');
+            });
+    };
+
+    $scope.onConnect = function () {
+        showLoadingIndicator('', 'Connecting ....');
+        $scope.terminal.connect()
+            .then(function () {
+                dismissLoadingIndicator();
+                goodToast('Connected...');
+            })
+            .catch(function (error) {
+                dismissLoadingIndicator();
+                console.error('Argh!', error, error.stack ? error.stack : '');
+                badToast('Unable to connect.');
+            });
+    }
+
+    if (!navigator.bluetooth) {
+        badToast('Bluetooth not supported, which is required.');
     } else if (navigator.bluetooth.referringDevice) {
-        $scope.connectClick();
+        $scope.onConnect();
     }
 
 });
 
+app.filter('range', function () {
+    return function (input, total) {
+        total = parseInt(total);
+
+        for (var i = 0; i < total; i++) {
+            input.push(i);
+        }
+
+        return input;
+    };
+});
+
+function SettingsController($scope, $mdDialog, terminalService) {
+
+    $scope.terminal = terminalService;
+
+    if ($scope.terminal.configBuffer) {
+        $scope.rx = $scope.terminal.configBuffer.getUint8(0);
+        $scope.tx = $scope.terminal.configBuffer.getUint8(1);
+        $scope.rts = $scope.terminal.configBuffer.getUint8(2);
+        $scope.cts = $scope.terminal.configBuffer.getUint8(3);
+
+        var num = $scope.terminal.configBuffer.getUint8(4) & 0xFF;
+        $scope.parity = num & 0xF;
+        $scope.flowControl = num >> 4;
+
+        $scope.baudRate = $scope.terminal.configBuffer.getUint8(5);
+    } else {
+        $scope.rx = 0;
+        $scope.tx = 0;
+        $scope.rts = 0;
+        $scope.cts = 0;
+        $scope.parity = 0;
+        $scope.flowControl = 0;
+        $scope.baudRate = 0;
+    }
+
+    //hard code to avoid changing them, disable in UI too.
+    $scope.rx = 5;
+    $scope.tx = 6;
+    $scope.rts = 0;
+    $scope.cts = 0;
+
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
+
+    $scope.submitClick = function () {
+        var cfgData = [];
+        cfgData.push($scope.rx);
+        cfgData.push($scope.tx);
+        cfgData.push($scope.rts);
+        cfgData.push($scope.cts);
+        cfgData.push($scope.flowControl << 4 | $scope.parity);
+        cfgData.push($scope.baudRate);
+
+        $scope.terminal.writeConfigData(new Uint8Array(cfgData))
+            .then(function () {
+                $scope.cancel();
+            });
+    };
+}
--- terminal.js
+++ terminal.js
@@ -1,136 +1,94 @@
-(function() {
-  "use strict";
+"use strict";
 
-    var util = new Util();
+var Terminal = function () {
 
-    var PUBLIC_SERVICE_UUID   = "50270001-DF25-45B0-8AD9-B27CEBA6622F";
+    var TERM_SERVICE_UUID = "50270001-df25-45b0-8ad9-b27ceba6622f";
 
-    var WRITE_CHAR_UUID   = "50270002-DF25-45B0-8AD9-B27CEBA6622F";
-    var NOTIFY_CHAR_UUID  = "50270003-DF25-45B0-8AD9-B27CEBA6622F";
-    var CONFIG_CHAR_UUID  = "50270004-DF25-45B0-8AD9-B27CEBA6622F";
+    var WRITE_CHAR_UUID = "50270002-df25-45b0-8ad9-b27ceba6622f";
+    var NOTIFY_CHAR_UUID = "50270003-df25-45b0-8ad9-b27ceba6622f";
+    var CONFIG_CHAR_UUID = "50270004-df25-45b0-8ad9-b27ceba6622f";
 
-  class terminal {
-    constructor() {
-      this.configData = '';
-      this.terminaldata = '';
-      this.characteristics = new Map();
-      
-      if(!navigator.bluetooth) {
-        console.error('bluetooth not supported');
-      }
+    function Terminal(bluetooth) {
+        this.connected = false;
+        this.writeCharacteristic = undefined;
+        this.configCharacteristic = undefined;
+        this.configBuffer = undefined;
+        this.bluetooth = bluetooth;
     }
 
-    connect() {
-      var options = {filters:[{name: 'BluTerm',}]};
-      return navigator.bluetooth.requestDevice(options).then(function (device) {
-        window.terminal.device = device;
-        return device.connectGATT();
-      }).then(function (server) {
-        window.terminal.server = server;
-        return Promise.all([
-          server.getPrimaryService(PUBLIC_SERVICE_UUID)
-          .then(function (service) {
-          return Promise.all([
-            window.terminal._cacheCharacteristic(service, WRITE_CHAR_UUID),
-            window.terminal._cacheCharacteristic(service, NOTIFY_CHAR_UUID),
-            window.terminal._cacheCharacteristic(service, CONFIG_CHAR_UUID),
-            service.getCharacteristic(NOTIFY_CHAR_UUID)
-            .then(function (characteristic) {
-              characteristic.addEventListener('characteristicvaluechanged', window.terminal.onNotify);
-              return characteristic.startNotifications();
-            })]);
-        })]);
-      });
+    Terminal.prototype.connect = function connect() {
+
+        var self = this;
+
+        var options = {filters: [{services: [TERM_SERVICE_UUID]}]};
+
+        return this.bluetooth.requestDevice(options)
+            .then(function (device) {
+                return device.gatt.connect();
+            })
+            .then(function (server) {
+                return server.getPrimaryService(TERM_SERVICE_UUID)
+            })
+            .then(function (service) {
+                return Promise.all([
+                    service.getCharacteristic(CONFIG_CHAR_UUID)
+                        .then(function (characteristic) {
+                            self.configCharacteristic = characteristic;
+                            return self.configCharacteristic.readValue()
+                                .then(function (value) {
+                                    self.configBuffer = value;
+                                });
+                        }),
+                    service.getCharacteristic(WRITE_CHAR_UUID)
+                        .then(function (characteristic) {
+                            self.writeCharacteristic = characteristic;
+                        }),
+                    service.getCharacteristic(NOTIFY_CHAR_UUID)
+                        .then(function (characteristic) {
+                            return characteristic.startNotifications()
+                                .then(function () {
+                                    characteristic.addEventListener('characteristicvaluechanged', function (event) {
+                                        //todo: generate event
+                                        if (self.updateUI) {
+                                            self.updateUI(event.target.value);
+                                        }
+                                    });
+                                });
+                        })
+                ]);
+            })
+            .then(function () {
+                self.connected = true;
+            });
     }
 
-    /* Smart Light Services */
-
-    writeData(hex_data){
-      var data = util.convertHexStringToNumArray(hex_data);
-      return window.terminal._writeCharacteristicValue(WRITE_CHAR_UUID, new Uint8Array(data));
-    }
-
-    getConfigData(){
-        return window.terminal._readCharacteristicValue(CONFIG_CHAR_UUID)
-      .then(window.terminal._configDataConvert);
-    }
-
-    onNotify(event) {
-        var characteristic = event.target;
-
-        var hex = '';
-        var arr = [];
-        for (var i = 0; i < characteristic.value.byteLength; ++i) {
-            var hexChar = characteristic.value.getUint8(i).toString(16);
-            if (hexChar.length == 1) {
-                hexChar = '0' + hexChar;
-            }
-            hex += hexChar;
-            arr.push(characteristic.value.getUint8(i));
+    Terminal.prototype.writeData = function writeData(sendData) {
+        if (this.writeCharacteristic) {
+            return this.writeCharacteristic.writeValue(sendData);
         }
-        window.terminal.terminaldata = window.terminal.terminaldata + hex;
-        window.terminal.updateUI();
+
+        return Promise.reject();
     }
 
-    /* Utils */
+    Terminal.prototype.writeConfigData = function writeConfigData(cfgData) {
+        var self = this;
 
-    _cacheCharacteristic(service, characteristicUuid) {
-      return service.getCharacteristic(characteristicUuid).then(function (characteristic) {
-        window.terminal.characteristics.set(characteristicUuid, characteristic);
-      });
-    }
-
-    _readCharacteristicValue(characteristicUuid) {
-      var characteristic = window.terminal.characteristics.get(characteristicUuid);
-      return characteristic.readValue().then(function (value) {
-        value = value.buffer ? value : new DataView(value);
-        return value;
-      });
-    }
-
-    _writeCharacteristicValue(characteristicUuid, value) {
-      var characteristic = window.terminal.characteristics.get(characteristicUuid);
-      if (window.terminal._debug) {
-        console.debug('WRITE', characteristic.uuid, value);
-      }
-      return characteristic.writeValue(value);
-    }
-
-    _configDataConvert(data){
-      var value = '';
-      for (var i = 0; i < data.byteLength; i++) {
-        value = value +''+ data.getUint8(i);
-      };
-      value = value.trim();
-      return value;
-    }
-  }
-
-  window.terminal = new terminal();
-
-})();
-
-function Util()
-{
-    this.stringToDecArray = function(str){
-        var dec, i;
-        var dec_arr = [];
-        if(str){
-            for (i=0; i<str.length; i++) {
-                dec = str.charCodeAt(i).toString(10);
-                dec_arr.push(Number(dec));
-            }
+        if (this.configCharacteristic) {
+            return this.configCharacteristic.writeValue(cfgData)
+                .then(function () {
+                    return self.configCharacteristic.readValue()
+                        .then(function (value) {
+                            self.configBuffer = value;
+                            return Promise.resolve();
+                        });
+                });
         }
-        return dec_arr;
-    };
+    }
 
-    this.convertHexStringToNumArray = function(str) {
-        var result = [];
-        for (var i = 0; i < str.length; i = i+2) {
-            result.push(parseInt(str[i]+str[i+1], 16));
-        }
-        return(result);
-    };
-    
-    return this;
+    return Terminal;
+
+}();
+
+if(window === undefined) {
+    module.exports.Terminal = Terminal;
 }
